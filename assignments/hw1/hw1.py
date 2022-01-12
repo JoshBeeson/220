209999cb4a60ba699a77df700404b93131487601
JoshBeeson <beesonjr@g.cofc.edu>
2022-01-12 15:21:54 -0500

    huh

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -9,26 +9,48 @@ Certification of Authenticity:
 I certify that this assignment is entirely my own work.
 I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
+from graphics import *
+
 
 
 def calc_rec_area():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    area = length * width
+    print("Area =", area)
+
 
 
 def calc_volume():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    height = eval(input("Enter the height: "))
+    volume = length * height * width
+    print(volume)
+
 
 
 def shooting_percentage():
-    pass
+    total_shots = eval(input("Input the total shots taken: "))
+    shots_made = eval(input("input the total shots made: "))
+    shooting_percent = (shots_made/total_shots) * 100
+
+    print(shooting_percent, " %")
+
 
 
 def coffee():
-    pass
+    pounds = eval(input("How many pounds of coffee would you like?"))
+    total_cost = float((10.50 * pounds) + (0.86 * pounds) + 1.5)
+    print("Your total is: ", total_cost, "$")
+
 
 
 def kilometers_to_miles():
-    pass
+    kilometers = eval(input("How many Kilometers did you travel? "))
+    miles = round(float(kilometers / 1.61), 2)
+    print("That's ", miles, " miles!")
+
 
 
 if __name__ == '__main__':
