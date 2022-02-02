61fd5be55dbb904f199e7f2a0e7b771841f723ce
JoshBeeson <beesonjr@g.cofc.edu>
2022-02-02 14:52:29 -0500

    term = eval(input("input the term"))     acc = 1     for i in range(term):         num = 2 + (i//2 * 2)         denom = 1 + ((i+1)//2 * 2)         acc = acc * (num/denom)     print(acc * 2)

--- assignments/hw3/hw3.py
+++ assignments/hw3/hw3.py
@@ -1,34 +1,54 @@
-"""
-Name: <your name goes here – first and last>
-<ProgramName>.py
-
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
-
-Certification of Authenticity:
-<include one of the following>
-I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
-"""
+"""Name: Joshua Bee son
+<hw1.py>
+Problem:f This program solves the issue of having to manually calculate several simple equations.
+I certify that this assignment is entirely my own work."""
 
 
 def average():
-    pass
+    grades = eval(input("how many grades will you enter? "))
+    acc = 0
+    for i in range(grades):
+        enter = eval(input("Enter grade: "))
+        acc = float(acc + enter)
+    print("average is ", (acc/grades))
+
 
 
 def tip_jar():
-    pass
+    acc = 0
+    for i in range(5):
+        money = eval(input("how much would you like to donate? "))
+        acc = float(acc + money)
+    print("total tips: ",acc)
+
 
 
 def newton():
-    pass
+    num = eval(input("What number do you want to square root? "))
+    improve = eval(input("How many times should we improve the approximation? "))
+    approx = num
+    for i in range(improve):
+        approx = float(((approx + (num/approx))/2))
+    print("the square root is approximately ", approx)
 
 
 def sequence():
-    pass
-
+    terms = eval(input("how many terms would you like? "))
+    list = []
+    for i in range(1,terms + 1, 2):
+        list.append(i)
+        list.append(i)
+    print(list[:terms])
 
 def pi():
-    pass
+    term = eval(input("how many terms in the series?"))
+    acc = 1
+    for i in range(term):
+        num = 2 + (i // 2 * 2)
+        denom = 1 + ((i + 1) // 2 * 2)
+        acc = acc * (num / denom)
+    print(acc * 2)
+
 
 
 if __name__ == '__main__':
